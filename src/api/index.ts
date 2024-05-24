8fd03a1d7ba2ca4f2e63771982fb57dc17958908
moon-ic <3276817499@qq.com>
2024-05-24 21:20:10 +0800

    fix: login冲突合并

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,11 @@
 import request from "../utils/request";
+import { adminReq, adminRes } from "@/types/login";
+
+/** @description 管理员登录 */
+export const login_admin = async (params: adminReq): Promise<adminRes> => {
+	const res = await request.post("/admin/login", params);
+	return res.data;
+};
 
 export const login = (accountType, username, password) => {
 	return request({
