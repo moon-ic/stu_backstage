aa07549232926a5efc496277e91093a8ffba1e56
moon-ic <3276817499@qq.com>
2024-05-27 14:20:44 +0800

    feat: 登出

--- src/components/header.vue
+++ src/components/header.vue
@@ -41,6 +41,7 @@ import { onMounted } from 'vue';
 import { useSidebarStore } from '../store/sidebar';
 import { useRouter } from 'vue-router';
 import imgurl from '../assets/img/img.jpg';
+import { logout_admin } from '@/api';
 
 const username: string | null = localStorage.getItem('vuems_name');
 const message: number = 2;
@@ -61,6 +62,7 @@ onMounted(() => {
 const router = useRouter();
 const handleCommand = (command: string) => {
     if (command == 'loginout') {
+        logout_admin();
         localStorage.removeItem('vuems_name');
         router.push('/login');
     } else if (command == 'user') {
